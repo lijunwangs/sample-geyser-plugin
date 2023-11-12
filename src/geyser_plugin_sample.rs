0d7db7e75ec7dfea9310bd21f5f9639ebb2a845d
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-11-11 17:03:46 -0800

    debug msg for more notifications

--- src/geyser_plugin_sample.rs
+++ src/geyser_plugin_sample.rs
@@ -1,6 +1,11 @@
 use log::*;
 /// Main entry for the Sample plugin
-use solana_geyser_plugin_interface::geyser_plugin_interface::{GeyserPlugin, ReplicaBlockInfoVersions, Result, SlotStatus};
+use solana_geyser_plugin_interface::geyser_plugin_interface::{
+    GeyserPlugin, ReplicaAccountInfoVersions, ReplicaBlockInfoVersions, ReplicaEntryInfoVersions,
+    ReplicaTransactionInfoVersions, Result, SlotStatus,
+};
+
+use solana_sdk::clock::Slot;
 
 #[derive(Default)]
 pub struct GeyserPluginSample {}
@@ -11,6 +16,17 @@ impl GeyserPlugin for GeyserPluginSample {
     }
     fn on_unload(&mut self) {}
 
+    #[allow(unused_variables)]
+    fn update_account(
+        &self,
+        account: ReplicaAccountInfoVersions,
+        slot: Slot,
+        is_startup: bool,
+    ) -> Result<()> {
+        info!("Got account notification");
+        Ok(())
+    }
+
     fn update_slot_status(&self, slot: u64, parent: Option<u64>, status: SlotStatus) -> Result<()> {
         info!("Updating slot {slot:?} at with status {status:?} of parent {parent:?}");
         Ok(())
@@ -19,6 +35,44 @@ impl GeyserPlugin for GeyserPluginSample {
     fn notify_block_metadata(&self, _blockinfo: ReplicaBlockInfoVersions) -> Result<()> {
         info!("Got block metadata");
         Ok(())
+    }
+
+    /// Called when an entry is executed.
+    #[allow(unused_variables)]
+    fn notify_entry(&self, entry: ReplicaEntryInfoVersions) -> Result<()> {
+        info!("Got entry notification");
+        Ok(())
+    }
+
+    #[allow(unused_variables)]
+    fn notify_transaction(
+        &self,
+        transaction: ReplicaTransactionInfoVersions,
+        slot: Slot,
+    ) -> Result<()> {
+        info!("Got txn notification");
+        Ok(())
+    }
+
+    /// Check if the plugin is interested in account data
+    /// Default is true -- if the plugin is not interested in
+    /// account data, please return false.
+    fn account_data_notifications_enabled(&self) -> bool {
+        true
+    }
+
+    /// Check if the plugin is interested in transaction data
+    /// Default is false -- if the plugin is interested in
+    /// transaction data, please return true.
+    fn transaction_notifications_enabled(&self) -> bool {
+        true
+    }
+
+    /// Check if the plugin is interested in entry data
+    /// Default is false -- if the plugin is interested in
+    /// entry data, return true.
+    fn entry_notifications_enabled(&self) -> bool {
+        true
     }
 }
 
